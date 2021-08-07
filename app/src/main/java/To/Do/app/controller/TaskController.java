8201137454923dbea155f53f523261081f2248e9
mikekang47 <gyeong0308@gmail.com>
2021-08-07 17:17:02 +0900

    Implemented Task CRUD

--- app/src/main/java/To/Do/app/controller/TaskController.java
+++ app/src/main/java/To/Do/app/controller/TaskController.java
@@ -2,6 +2,7 @@ package To.Do.app.controller;
 
 import To.Do.app.dto.Task;
 import To.Do.app.exception.TaskNotFoundException;
+import To.Do.app.service.TaskService;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -11,66 +12,42 @@ import java.util.List;
 @RequestMapping("/tasks")
 @RestController
 public class TaskController {
+    TaskService taskService;
 
-    List<Task> tasks = new ArrayList<>();
-    Long id = 0L;
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
 
     @GetMapping
     List<Task> showAll() {
-        return tasks;
+        return taskService.getTasks();
     }
 
     @GetMapping("{id}")
     Task detail(@PathVariable Long id) {
-        return getTask(id);
+        return taskService.getTask(id);
     }
 
     @PostMapping
     @ResponseStatus(HttpStatus.OK)
-    Task create(@RequestBody Task source) {
-        Task task = new Task();
-        task.setTitle(source.getTitle());
-        task.setId(generateId());
-
-        tasks.add(task);
-
-        return task;
+    Task createTask(@RequestBody Task source) {
+        return taskService.create(source);
     }
 
     @PutMapping("{id}")
-    Task put(@PathVariable Long id, @RequestBody Task source) {
-        Task task = getTask(id);
-
-        task.setTitle(source.getTitle());
-
-        return task;
+    Task putTask(@PathVariable Long id, @RequestBody Task source) {
+        return taskService.update(id, source);
     }
 
     @PatchMapping("{id}")
-    Task patch(@PathVariable Long id, @RequestBody Task source) {
-        Task task = getTask(id);
-        task.setTitle(source.getTitle());
-
-        return task;
+    Task patchTask(@PathVariable Long id, @RequestBody Task source) {
+        return taskService.update(id, source);
     }
 
     @DeleteMapping("{id}")
     void delete(@PathVariable Long id) {
-        Task task = getTask(id);
-        tasks.remove(task);
+        taskService.delete(id);
     }
 
 
-    Task getTask(Long id) {
-        return tasks.stream()
-                .filter(task -> task.getId().equals(id))
-                .findFirst()
-                .orElseThrow(() -> new TaskNotFoundException(id));
-    }
-
-
-   private Long generateId() {
-        id += 1L;
-        return id;
-    }
 }
--- app/src/main/java/To/Do/app/service/TaskService.java
+++ app/src/main/java/To/Do/app/service/TaskService.java
@@ -1,4 +1,54 @@
 package To.Do.app.service;
 
+import To.Do.app.dto.Task;
+import To.Do.app.exception.TaskNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class TaskService {
+
+    Long id = 0L;
+    
+    public List<Task> tasks = new ArrayList<>();
+
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public Task getTask(Long id) {
+        return tasks.stream()
+                .filter((task) -> task.getId().equals(id))
+                .findFirst()
+                .orElseThrow(() -> new TaskNotFoundException(id));
+    }
+
+    public Task create(Task source) {
+        Task task = new Task();
+        task.setTitle(source.getTitle());
+        task.setId(generateId());
+
+        tasks.add(task);
+
+        return task;
+    }
+
+    public Task update(Long id, Task source) {
+        Task task = getTask(id);
+
+        task.setTitle(source.getTitle());
+
+        return task;
+    }
+
+    public void delete(Long id) {
+        Task task = getTask(id);
+        tasks.remove(task);
+    }
+
+    public Long generateId() {
+        id += 1L;
+        return id;
+    }
+
 }
